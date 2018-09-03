5507e109463a1e09d6860d6cdc61a74eb6660951
Mark Lewis <thnkloud9@gmail.com>
2018-09-03 21:49:04 +0200

    checks letter order after checking length and unique chars

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ request({
         if (err) throw err;
         zipfile.readEntry();
         zipfile.on('entry', (entry) => {
-            let word = '';
+            var word = '';
             zipfile.openReadStream(entry, (err, readStream) => {
                 if (err) throw err;
                 readStream.on('data', (data) => {
@@ -33,9 +33,9 @@ request({
 });
 
 function processWords(words) {
-    let wordsArray = words.split('\n');
-    let wordMetaObjects = [];
-    let friends = 0;
+    var wordsArray = words.split('\n');
+    var wordMetaObjects = [];
+    var friends = 0;
     wordsArray.forEach((word) => {
         let uniq = '';
         for (var i = 0; i < word.length; i++) {
@@ -55,13 +55,49 @@ function processWords(words) {
             if ((compareWord.word !== word.word) &&
           (compareWord.length === word.length) &&
           (compareWord.uniq === word.uniq)) {
-                friends++;
+                let wordLetters = getLetterCount(word.word);
+                let compareWordLetters = getLetterCount(compareWord.word);
+
+                if (arraysEqual(Object.values(wordLetters), Object.values(compareWordLetters))) {
+                    console.log('match found', word, compareWord);
+                    friends++;
+                    return true;
+                }
                 return true;
             }
         });
     });
 
     process.stdout.write('Found ' + friends + ' words with at least 1 friend.  Out of a total of ' + words.length + ' words.');
+}
+
+function getLetterCount(word) {
+    var letters = new Object;
+
+    var count = 1;
+    for(let x = 0, length = word.length; x < length; x++) {
+        var letter = word.charAt(x);
+        var nextLetter = (x < (length-1)) ? word.charAt(x+1) : '';
+        if (letter === nextLetter) {
+           count++;
+        } else {
+            letters[x] = count;
+            count = 1;
+        }
+    }
+
+    return letters;
+}
+
+function arraysEqual(arr1, arr2) {
+    if(arr1.length !== arr2.length)
+        return false;
+    for(var i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i])
+            return false;
+    }
+
+    return true;
 }
 
 
